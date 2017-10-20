6d6bf9fdc497e19c3e5c853738077668b4c17ae7
Eaknyt <deaknyt@gmail.com>
2017-10-20 20:14:06 +0200

    Add a ParticleEffect class

--- particleeffect.cpp
+++ particleeffect.cpp
@@ -1,15 +1,77 @@
 #include "particleeffect.h"
 
 
-ParticleEffect::ParticleEffect(const QVector3D &pos, int count, unsigned int life) :
+namespace {
+
+const int m_countDefaultValue = 20;
+const unsigned int m_maxLifeDefaultValue = 50;
+const float m_radiusDefaultValue = 50.f;
+const QVector3D m_directionDefaultValue = {0, 1, 0};
+const float m_speedDefaultValue = 0.4f;
+const float m_particlesSizeDefaultValue = 4.f;
+
+} // anon namespace
+
+
+ParticleEffect::ParticleEffect() :
+    m_randDevice(),
+    m_randEngine(m_randDevice()),
+    m_radiusXRandDistrib(),
+    m_radiusZRandDistrib(),
+    m_count(m_countDefaultValue),
+    m_maxLife(m_maxLifeDefaultValue),
+    m_worldPos(),
+    m_radius(m_radiusDefaultValue),
+    m_direction(m_directionDefaultValue),
+    m_speed(m_speedDefaultValue),
+    m_particlesSize(m_particlesSizeDefaultValue),
+    m_worldPositions(m_count, m_worldPos),
+    m_lifes(m_count, m_maxLife)
+{
+    resetRadiusRandDistribs();
+}
+
+ParticleEffect::ParticleEffect(const QVector3D &pos, int count, unsigned int maxLife) :
+    m_randDevice(),
+    m_randEngine(m_randDevice()),
+    m_radiusXRandDistrib(),
+    m_radiusZRandDistrib(),
     m_count(count),
-    m_life(life),
+    m_maxLife(maxLife),
     m_worldPos(pos),
-    m_direction(0, 1, 0),
-    m_speed(4.f),
-    m_worldPositions(count),
-    m_lifes(count)
-{}
+    m_radius(m_radiusDefaultValue),
+    m_direction(m_directionDefaultValue),
+    m_speed(m_speedDefaultValue),
+    m_particlesSize(m_particlesSizeDefaultValue),
+    m_worldPositions(m_count, m_worldPos),
+    m_lifes(m_count, m_maxLife)
+{
+    resetRadiusRandDistribs();
+}
+
+QVector3D ParticleEffect::worldPos() const
+{
+    return m_worldPos;
+}
+
+void ParticleEffect::setWorldPos(const QVector3D &pos)
+{
+    m_worldPos = pos;
+}
+
+float ParticleEffect::radius() const
+{
+    return m_radius;
+}
+
+void ParticleEffect::setRadius(float radius)
+{
+    if (m_radius != radius) {
+        m_radius = radius;
+
+        resetRadiusRandDistribs();
+    }
+}
 
 QVector3D ParticleEffect::direction() const
 {
@@ -19,26 +81,6 @@ QVector3D ParticleEffect::direction() const
 void ParticleEffect::setDirection(const QVector3D &direction)
 {
     m_direction = direction.normalized();
-}
-
-void ParticleEffect::live(float dt)
-{
-    for (int i = 0; i < m_lifes.size(); i++) {
-        unsigned int &life = m_lifes[i];
-        QVector3D &particlePos = m_worldPositions[i];
-
-        // Recycle particle
-        if (life == 0) {
-            life = m_life;
-            particlePos = m_worldPos;
-        }
-        // or animate and decrase life
-        else if (life < 0) {
-            life--;
-
-            particlePos += m_speed * direction() * dt;
-        }
-    }
 }
 
 float ParticleEffect::speed() const
@@ -51,12 +93,57 @@ void ParticleEffect::setSpeed(float speed)
     m_speed = speed;
 }
 
-QVector3D ParticleEffect::worldPos() const
+float ParticleEffect::particlesSize() const
 {
-    return m_worldPos;
+    return m_particlesSize;
 }
 
-void ParticleEffect::setWorldPos(const QVector3D &pos)
+void ParticleEffect::setParticlesSize(float particlesSize)
 {
-    m_worldPos = pos;
+    m_particlesSize = particlesSize;
+}
+
+void ParticleEffect::live(float dt)
+{
+    for (int i = 0; i < m_lifes.size(); i++) {
+        unsigned int &life = m_lifes[i];
+        QVector3D &particlePos = m_worldPositions[i];
+
+        // Recycle particle
+        if (life == 0) {
+            life = m_maxLife;
+            particlePos = m_worldPos;
+        }
+        // Sets the particle's starting position
+        else if (life == m_maxLife) {
+            particlePos.setX(m_radiusXRandDistrib(m_randEngine));
+            particlePos.setZ(m_radiusZRandDistrib(m_randEngine));
+
+            life--;
+        }
+        // or animate and decrase life
+        else {
+            life--;
+
+            //TODO parametrize that by adding a std::function member attribute ?
+            particlePos += m_speed * direction() * dt;
+        }
+    }
+}
+
+const std::vector<QVector3D> &ParticleEffect::worldPositions() const
+{
+    return m_worldPositions;
+}
+
+void ParticleEffect::resetRadiusRandDistribs()
+{
+    const float worldPosX = m_worldPos.x();
+    const float worldPosZ = m_worldPos.z();
+
+    m_radiusXRandDistrib = std::uniform_real_distribution<float>
+            (worldPosX - m_radius, worldPosX + m_radius);
+
+    m_radiusZRandDistrib = std::uniform_real_distribution<float>
+            (worldPosZ - m_radius, worldPosZ + m_radius);
 }
--- particleeffect.h
+++ particleeffect.h
@@ -1,6 +1,7 @@
 #ifndef PARTICLEEFFECT_H
 #define PARTICLEEFFECT_H
 
+#include <random>
 #include <vector>
 
 #include <QVector3D>
@@ -9,10 +10,14 @@
 class ParticleEffect
 {
 public:
-    ParticleEffect(const QVector3D &worldPos, int count, unsigned int life);
+    ParticleEffect();
+    ParticleEffect(const QVector3D &worldPos, int count, unsigned int maxLife);
 
     QVector3D worldPos() const;
     void setWorldPos(const QVector3D &worldPos);
+
+    float radius() const;
+    void setRadius(float radius);
 
     QVector3D direction() const;
     void setDirection(const QVector3D &direction);
@@ -20,15 +25,32 @@ public:
     float speed() const;
     void setSpeed(float speed);
 
+    float particlesSize() const;
+    void setParticlesSize(float particlesSize);
+
     void live(float dt);
 
+    const std::vector<QVector3D> &worldPositions() const;
+
 private:
+    void resetRadiusRandDistribs();
+
+private:
+    std::random_device m_randDevice;
+    std::default_random_engine m_randEngine;
+
+    std::uniform_real_distribution<float> m_radiusXRandDistrib;
+    std::uniform_real_distribution<float> m_radiusZRandDistrib;
+
     int m_count;
-    unsigned int m_life;
+    unsigned int m_maxLife;
 
     QVector3D m_worldPos;
+    float m_radius;
     QVector3D m_direction;
     float m_speed;
+    //TODO float m_count; (adapt m_worldPositions and m_lifes)
+    float m_particlesSize;
 
     std::vector<QVector3D> m_worldPositions;
     std::vector<unsigned int> m_lifes;
