4e4cc75d3965cd650354c4376d90d525eea5c4bd
Eaknyt <deaknyt@gmail.com>
2017-12-06 09:26:37 +0100

    editor.EntityxHook: hide hooks from interface

--- editor/entityxhook.cpp
+++ editor/entityxhook.cpp
@@ -20,6 +20,36 @@
 #include "script/script.h"
 
 
+template <typename C>
+class ComponentAddedHook :
+        public entityx::System<ComponentAddedHook<C>>,
+        public entityx::Receiver<ComponentAddedHook<C>>
+{
+public:
+    ComponentAddedHook(EntityxHook &entityxHook) :
+        m_entityxHook(entityxHook)
+    {}
+
+    void receive(const entityx::ComponentAddedEvent<C> &event)
+    {
+        Q_EMIT m_entityxHook.entityComponentAdded(event.entity);
+    }
+
+    void configure(entityx::EventManager &events) override
+    {
+        events.subscribe<entityx::ComponentAddedEvent<C>>(*this);
+    }
+
+    void update(entityx::EntityManager &entities,
+                entityx::EventManager &events,
+                double dt) override {}
+private:
+    EntityxHook &m_entityxHook;
+};
+
+
+////////////////////// EntityxHook //////////////////////
+
 EntityxHook::EntityxHook(SystemEngine &systemEngine, QObject *parent) :
     QObject(parent),
     m_systemEngine(systemEngine)
--- editor/entityxhook.h
+++ editor/entityxhook.h
@@ -23,32 +23,4 @@ private:
     SystemEngine &m_systemEngine;
 };
 
-
-template <typename C>
-class ComponentAddedHook :
-        public entityx::System<ComponentAddedHook<C>>,
-        public entityx::Receiver<ComponentAddedHook<C>>
-{
-public:
-    ComponentAddedHook(EntityxHook &entityxHook) :
-        m_entityxHook(entityxHook)
-    {}
-
-    void receive(const entityx::ComponentAddedEvent<C> &event)
-    {
-        Q_EMIT m_entityxHook.entityComponentAdded(event.entity);
-    }
-
-    void configure(entityx::EventManager &events) override
-    {
-        events.subscribe<entityx::ComponentAddedEvent<C>>(*this);
-    }
-
-    void update(entityx::EntityManager &entities,
-                entityx::EventManager &events,
-                double dt) override {}
-private:
-    EntityxHook &m_entityxHook;
-};
-
 #endif // ENTITYXHOOK_H
